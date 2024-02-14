16c91b98acf7bac537a8fc9ea3ecf2acba99b59e
k <a@a.com>
2024-02-13 21:40:15 -0600

    stuff

--- commands/server/logging/main.py
+++ commands/server/logging/main.py
@@ -1,9 +1,11 @@
 import discord
+from datetime import datetime
 
 from discord.ext import commands
 class MainCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        a = bot
 
     @commands.command(name = "logging")
     async def logging(self, ctx, t):
@@ -15,3 +17,7 @@ class MainCog(commands.Cog):
 
         if t == "enable":
             await commands.server.logging.enable.main(ctx)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        await message.channel.send(f"message deleted @ {datetime.now().strftime('%H:%M:%S UTC')}\n\ncontent: {message.content}\nchannel: {message.channel}")
