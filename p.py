367446aa220cbcbee4f579ca99e0fb5cffde0286
k <rvyeinq@gmail.com>
2024-02-14 08:33:28 -0600

    Update p.py

--- p.py
+++ p.py
@@ -1,13 +1,1 @@
-x = open(f"misc/logging.isEnabled/1204867121364996127.txt", 'r+')
-print(x.readline())
-if (str(x.readline())) == "false":
-    print(x.readline())
-    x.truncate(0)
-    x.seek(0)
 
-    x.write('1')
-
-    print('logging enabled. #logs')
-
-if (x.readline()) == 'true':
-    print('logging is already enabled! #logs')
