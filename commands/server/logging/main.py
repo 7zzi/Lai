07971bef2ad7763de94043cbab7b2000147af2e4
k <rvyeinq@gmail.com>
2024-02-14 08:35:14 -0600

    Update main.py

--- commands/server/logging/main.py
+++ commands/server/logging/main.py
@@ -5,11 +5,11 @@ from discord.ext import commands
 class MainCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        a = bot
+        
 
     @commands.command(name = "logging")
     @commands.has_guild_permissions(manage_messages = True)
-    async def logging(self, ctx, t):
+    async def logging(self, ctx, t, *, c = None):
         import commands.server.logging.disable
         import commands.server.logging.enable
         
@@ -17,13 +17,19 @@ class MainCog(commands.Cog):
             await commands.server.logging.disable.main(ctx)
 
         if t == "enable":
-            await commands.server.logging.enable.main(ctx)
+            await commands.server.logging.enable.main(ctx, c)
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        x = open(f"misc/logging.isEnabled/{ctx.guild.id}.txt", 'r+')
-        r = x.read()
+    async def on_message_delete(self, ma):
+        e = open(f"misc/logging.isEnabled/{ma.guild.id}.txt", 'r+')
+        et = e.read()
 
-        if (r) == '1':
-            await message.channel.send(f"message deleted @ {datetime.now().strftime('%H:%M:%S UTC')}\n\ncontent: {message.content}\nchannel: {message.channel}")
-        x.close()
+        cf = open(f"misc/logging.channel/{ma.guild.id}.txt", 'r')
+        c = str(cf.read()).replace("<#", '').replace(">", '')
+
+        logChannel = await discord.utils.get_or_fetch(ma.guild, 'channel', c, default=None)
+
+        if (et) == '1':
+            await logChannel.send(f"message deleted @ {datetime.now().strftime('%H:%M:%S UTC')}\n\ncontent: {ma.content}\nchannel: {ma.channel.mention}\nauthor: {ma.author.mention}")
+        
+        e.close()
